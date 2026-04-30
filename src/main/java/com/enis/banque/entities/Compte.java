e076cf94c1f79f545dae21b31ead37e01241e018
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-04-30 03:49:22 +0200

    Amélioration de l'entité Compte

--- src/main/java/com/enis/banque/entities/Compte.java
+++ src/main/java/com/enis/banque/entities/Compte.java
@@ -1,20 +1,32 @@
 package com.enis.banque.entities;
 
+import com.enis.banque.entities.Client;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "comptes")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Compte {
+
     @Id
+    @NotEmpty(message = "Le RIB est obligatoire")
+    @Size(min = 5, max = 30, message = "Le RIB doit contenir entre 5 et 30 caractères")
+    @Column(length = 30, unique = true)
     private String rib;
 
+    @PositiveOrZero(message = "Le solde ne peut pas être négatif")
+    @Column(nullable = false)
     private double solde;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
     private Client client;
 }
-
