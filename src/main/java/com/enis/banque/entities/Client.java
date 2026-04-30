ea1032838d7bbb3025baa0cd8ff83353fdf49165
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-04-30 03:48:46 +0200

    Amélioration de l'entité Client

--- src/main/java/com/enis/banque/entities/Client.java
+++ src/main/java/com/enis/banque/entities/Client.java
@@ -1,20 +1,36 @@
 package com.enis.banque.entities;
+
+import com.enis.banque.entities.Compte;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "clients")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Client {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "Le nom est obligatoire")
+    @Size(min = 2, max = 50, message = "Le nom doit contenir entre 2 et 50 caractères")
+    @Column(nullable = false, length = 50)
     private String nom;
+
+    @NotEmpty(message = "Le prénom est obligatoire")
+    @Size(min = 2, max = 50, message = "Le prénom doit contenir entre 2 et 50 caractères")
+    @Column(nullable = false, length = 50)
     private String prenom;
 
-    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
-    private List<Compte> comptes;
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Compte> comptes = new ArrayList<>();
 }
