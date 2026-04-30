6cf05310d12bcd4585f7f8036f6d7a6e70feae84
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-04-30 03:52:35 +0200

    Amélioration du depôt Client

--- src/main/java/com/enis/banque/repositories/ClientRepository.java
+++ src/main/java/com/enis/banque/repositories/ClientRepository.java
@@ -3,11 +3,14 @@ package com.enis.banque.repositories;
 import com.enis.banque.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    @Query("SELECT c FROM Client c WHERE LOWER(c.nom) LIKE LOWER(CONCAT('%', :kw, '%')) " +
-            "OR LOWER(c.prenom) LIKE LOWER(CONCAT('%', :kw, '%'))")
-    List<Client> search(String kw);
+    @Query("SELECT c FROM Client c WHERE LOWER(c.nom) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(c.prenom) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Client> searchByKeyword(@Param("keyword") String keyword);
 }
