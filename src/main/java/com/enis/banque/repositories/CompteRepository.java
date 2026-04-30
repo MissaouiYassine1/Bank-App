4afcbd7fa6c65e483404f957a01f3aba769b9969
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-04-30 03:53:09 +0200

    Amélioration du depôt Compte

--- src/main/java/com/enis/banque/repositories/CompteRepository.java
+++ src/main/java/com/enis/banque/repositories/CompteRepository.java
@@ -2,6 +2,12 @@ package com.enis.banque.repositories;
 
 import com.enis.banque.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface CompteRepository extends JpaRepository<Compte, String> {
+
+    List<Compte> findByClientId(Long clientId);
+    void deleteByRib(String rib);
 }
