054209447cfee5e7744e1f6be5bc32c3fd5c9707
MissaouiYassine1 <yassine.missaoui@enis.tn>
2026-04-30 03:50:33 +0200

    Ajout de DTO client

---
+++ src/main/java/com/enis/banque/dto/ClientDTO.java
@@ -0,0 +1,23 @@
+package com.enis.banque.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDTO {
+
+    private Long id;
+
+    @NotEmpty(message = "Le nom est obligatoire")
+    @Size(min = 2, max = 50, message = "Le nom doit contenir entre 2 et 50 caractères")
+    private String nom;
+
+    @NotEmpty(message = "Le prénom est obligatoire")
+    @Size(min = 2, max = 50, message = "Le prénom doit contenir entre 2 et 50 caractères")
+    private String prenom;
+}
